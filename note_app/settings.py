f00562c628686bb541aa601aaa3dc0153b1c1ef0
Vkushwahaa <kushwahavinay62@gmail.com>
2024-09-30 01:28:42 +0530

    hope

--- note_app/settings.py
+++ note_app/settings.py
@@ -195,7 +195,7 @@ STATIC_URL = "static/"
 import os
 
 STATICFILES_DIRS = os.path.join(BASE_DIR,'frontend_note-app','dist'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static/assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
