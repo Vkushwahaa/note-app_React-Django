cda8538b224e187df9ba07ab56dfd68e693b8bad
Vkushwahaa <kushwahavinay62@gmail.com>
2024-10-01 01:07:51 +0530

    last

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-echo "Starting collectstatic process..."
-
-# Navigate to the Django project directory
-cd note_app || { echo "Failed to navigate to note_app directory"; exit 1; }
-
-# Run the collectstatic command
+echo "Running collectstatic..."
 python manage.py collectstatic --noinput
-
-if [ $? -eq 0 ]; then
-    echo "collectstatic completed successfully."
-else
-    echo "collectstatic failed."
-    exit 1
-fi
