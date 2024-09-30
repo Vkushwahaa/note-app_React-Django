ed99d877a46f8960b5d404882d727c6bd4546100
Vkushwahaa <kushwahavinay62@gmail.com>
2024-10-01 01:15:32 +0530

    l

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 echo "Checking Python installation..."
 which python3
+
+echo "Setting up virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
+echo "Installing Python dependencies..."
+pip install -r requirements.txt
+
 echo "Running collectstatic..."
 python3 manage.py collectstatic --noinput
