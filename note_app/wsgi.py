1383ab65d8459b786befca46804798ecbf442109
Vkushwahaa <kushwahavinay62@gmail.com>
2024-10-06 15:44:42 +0530

    post

--- note_app/wsgi.py
+++ note_app/wsgi.py
@@ -10,9 +10,17 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "note_app.settings")
 
+try:
+    from django import setup
+    setup()
+    call_command('migrate')  # Run migrations
+except Exception as e:
+    print(f"Error running migrations: {e}")
+    
 application = get_wsgi_application()
 
 app = application
