8955fd553730cc612a276f477105ac0a9db70e5d
Vkushwahaa <kushwahavinay62@gmail.com>
2024-10-06 16:01:22 +0530

    wsgi

--- note_app/wsgi.py
+++ note_app/wsgi.py
@@ -8,17 +8,25 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import logging
 
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "note_app.settings")
 
 try:
     from django import setup
     setup()
+    
+    # Log that we are about to run migrations
+    logger.info("Running migrations...")
+    
     call_command('migrate')  # Run migrations
-    print("ran successfully")
+    logger.info("Migrations ran successfully")
 except Exception as e:
     print(f"Error running migrations: {e}")
     
