5b679514aea959cf8484d92ae9c629e4937d75a8
Vkushwahaa <kushwahavinay62@gmail.com>
2024-10-06 16:14:55 +0530

    done

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "Installing dependencies"
-env/scripts/activate
 
 echo "Installing dependencies"
 python3 -m pip install -r requirements.txt
 
+echo "collect static"
+python3 manage.py collectstatic
+
 echo "Making migrations"
+python3 manage.py makemigrations
 python3 manage.py migrate
